3aab4d7bec675126bd60285674a8ab8ca5e3e79d
RobinKohrs <robin.kohrs@gmx.de>
2024-02-10 11:22:01 +0100

    legend cont

--- src/lib/ui/LegendSize.svelte
+++ src/lib/ui/LegendSize.svelte
@@ -2,7 +2,7 @@
   export let position = "right";
   export let scaleSize;
 
-  let circles = [{ val: 0.1 }, { val: 4 }];
+  let circles = [{ val: 0.1 }, { label: "Wave Height", val: 0 }, { val: 4 }];
 
   let svgLegendWidth;
   let circles_computed;
@@ -21,24 +21,29 @@
 
 {#if height}
   <div
-    class="legend-size-container flex w-full h-full items-center justify-center gap-4"
+    class="legend-size-container flex w-full h-full items-center justify-between px-3"
     bind:clientWidth={svgLegendWidth}
   >
     {#each circles_computed as circle, i}
       <div class="legend item flex items-center gap-2">
         {#if i === 0}
-          <span>0</span>
-        {/if}
-        <span
-          style:height={`${circle.r * 2}px`}
-          class="rounded-full bg-black aspect-square inline-block"
-        >
-        </span>
-        {#if i === 0}
-          <span class="underline">Swell-Rating</span>
-        {/if}
-        {#if i === 1}
-          <span>4</span>
+          <span class="relative flex items-center ml-16">
+            <span>0</span>
+            <span
+              style:height={`${circle.r * 2}px`}
+              class="rounded-full bg-black aspect-square absolute translate-x-full ml-2"
+            />
+          </span>
+        {:else if i === 1}
+          <span>{circle.label}</span>
+        {:else}
+          <span class="relative flex items-center mr-16">
+            <span>4</span>
+            <span
+              style:height={`${circle.r * 2}px`}
+              class="rounded-full border border-black aspect-square absolute -translate-x-[120%]"
+            />
+          </span>
         {/if}
       </div>
     {/each}
